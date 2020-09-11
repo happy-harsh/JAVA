ec9b201d8c629de6cb3d0a7e003bc81a2dd0c04f
HARSH DODIYA <70851344+happy-harsh@users.noreply.github.com>
2020-09-11 19:03:20 +0530

    Add files via upload

---
+++ prac1.java
@@ -0,0 +1,13 @@
+//JAVA program for addition of two integers
+public class prac1{
+
+    public static void main(String[] args) {
+        
+        int a= 10;
+        int  b= 20;
+
+        int sum = a + b;
+
+        System.out.println("The Addition of numbers is: " + sum);
+    }
+}
