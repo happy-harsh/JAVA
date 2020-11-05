6f00e0830ce72a30fb8db4ff9a2804b6e104dff4
HARSH DODIYA <70851344+happy-harsh@users.noreply.github.com>
2020-11-05 14:44:24 +0530

    Update prac3.java

--- prac3.java
+++ prac3.java
@@ -1,7 +1,7 @@
 // 2 accept marks from user,if marks greater than 40,declare the student as "pass" else "fail"
 
 import java.util.Scanner;
-public class prac2 {
+public class prac3 {
     public static void main(String[] args) {
         int s1;
         Scanner in =new Scanner(System.in);
