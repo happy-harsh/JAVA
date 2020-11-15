3570ee6391b43e7e81984d64f9e92e18ef79cbe2
HARSH DODIYA <70851344+happy-harsh@users.noreply.github.com>
2020-11-15 16:30:55 +0530

    Update prac4.java

--- prac4.java
+++ prac4.java
@@ -1,5 +1,5 @@
 // 4 Display sum of first 10 even numbers using do-while loop
-public class prac5 {
+public class prac4{
     public static void main(String[] args) {
         int x=0,sum=0;
        do{
