e24a57fe2131b83f4b14113134b9e55aedf9c872
HARSH DODIYA <70851344+happy-harsh@users.noreply.github.com>
2020-11-19 20:52:59 +0530

    Add files via upload

---
+++ prac7.java
@@ -0,0 +1,21 @@
+//7. Display sum of elements of array
+import java.util.Scanner;
+public class prac7
+{
+    public static void main(String[] args) 
+    {
+        int n, sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n]; // array declaration 
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+            sum = sum + a[i];
+        }
+        System.out.println("Sum:"+sum);
+        s.close();
+    }
+}
